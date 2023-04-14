684a921bab3530b746979a4a0efd1e866c0ecdab
kidstonb <113933033+kidstonb@users.noreply.github.com>
2023-04-14 14:17:31 -0400

    update Streamlit app

--- DeployFlask.py
+++ DeployFlask.py
@@ -1,8 +1,33 @@
-from flask import Flask 
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# https://www.youtube.com/watch?v=2LqrfEzuIMk
 # load model 
+# model = .load('')
+
+# Home page
+@app.route('/')
+def home():
+    result = ''
+    return render_template('index.html', **locals())
+
+
+@app.route('/predict', methods=['POST', 'GET'])
+def predict():
+    # input features 
+    # wineFeatures = request.form['wineFeatures']
+        # might need to convert to float?
+    # wineFeatures = float(request.form['wineFeatures'])
+    # continue with other inputs 
+        # results will be an array, index of [0]
+    # result = model.predict([[feature1, feature2, feature3]])[0]
+    return render_template('index.html', **locals())
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
 # route to take user input
 # pass input to model (preprocessing)
 # return predicted output
