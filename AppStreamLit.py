23b9e07db18b5f8f335e77fac920713407172478
kidstonb <113933033+kidstonb@users.noreply.github.com>
2023-04-16 16:46:23 -0400

    Major update to streamlit

--- AppStreamLit.py
+++ AppStreamLit.py
@@ -1,17 +1,47 @@
 import streamlit as st
 import pandas as pd
 import pickle
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 # from pathlib import Path
 
-# Load model file
+# Load model file for white
 # filename = Path('LogRDummyModel.pkl')
-model = pickle.load(open('LogRDummyModel.pkl', 'rb'))
+model_white = pickle.load(open('Resources/LogRDummyModelWhite.pkl', 'rb'))
+# Load model file for red
+model_red = pickle.load(open('Resources/LogRDummyModelRed.pkl', 'rb'))
 
-def predict(feature1, feature2, feature3):
-    # code insurance to limit responses to within data range
-    # Also to scale input appropriately 
+# Load and scale datasets, in order to scale new data coming in
+df_white = pd.read_csv('Resources/white_train_norm.csv', sep = ',')
+df_red = pd.read_csv('Resources/red_train_norm.csv', sep = ',')
+
+X_white = df_white.drop(columns = 'quality')
+X_red = df_red.drop(columns='quality')
+
+scaler_white = StandardScaler()
+scaler_red = StandardScaler()
+
+# Don't need to record the scaled data? Just prepare the scaler 
+# or use .transform instead of .fit?
+scaler_white.fit(X_white)
+scaler_red.fit(X_red)
+
+# Function to predict from model 
+def predict(user_input):
+
+    # Scale user input to pass to model, choosing appropriate model from wine colour
+    # Testing to see if colour button is working 
+    if colour == 'White':
+        user_input_scaled = scaler_white.transform([user_input])
+        model = model_white
+    elif colour == 'Red': 
+        user_input_scaled = scaler_red.transform([user_input])
+        model = model_red 
     
-    input = pd.DataFrame([[feature1, feature2, feature3]],columns=['feature1','feature2','feature3'])
+    # Convert input to a dataframe to pass to .predict method
+    input = pd.DataFrame([user_input_scaled],\
+                         columns=['Fixed Acidity','Volatile Acidity','Citric Acid','Residual Sugar','Chlorides','Free Sulfur Dioxide', 'Total Sulfur Dioxide',\
+                                  'Density', 'pH Level', 'Sulphates Content', 'Alcohol Content']) # str(num) for num in num_list?
     prediction = model.predict(input)
     return prediction
 
@@ -19,12 +49,28 @@ st.title('Wine Quality Prediction')
 st.image('https://images.unsplash.com/photo-1585553616435-2dc0a54e271d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8d2luZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=400&q=60')
 st.header('Enter wine chemistry below')
 
-# does value = 1.0 scale it?
-feature1 = st.number_input('feature1', min_value = 0.0, max_value = 5.0, value=1.0)
-feature2 = st.number_input('feature2', min_value = 0.0, max_value = 5.0, value=1.0)
-feature3 = st.number_input('feature3', min_value = 0.0, max_value = 5.0, value=1.0)
+# User feature input
+# Add units to these, maybe description or like Citric Acid content?
+feature1 = st.number_input('Fixed Acidity', min_value = 1.5, max_value = 2.5, value=2)
+feature2 = st.number_input('Volatile Acidity', min_value = 0.35, max_value = 1.15, value=0.5)
+feature3 = st.number_input('Citric Acid', min_value = 0.0, max_value = 0.95, value=0)
+feature4 = st.number_input('Residual Sugar', min_value = 0.8, max_value = 3.0, value=1.0)
+feature5 = st.number_input('Chlorides', min_value = 0.2, max_value = 0.53, value=0.3)
+feature6 = st.number_input('Free Sulfur Dioxide', min_value = 1.44, max_value = 4.8, value=2.5)
+feature7 = st.number_input('Total Sulfur Dioxide', min_value = 1.8, max_value = 259, value=5) # This one huge different b/w white and red, trying min/max of each
+feature8 = st.number_input('Density', min_value = 0.996, max_value = 1.001, value=0.997)
+feature9 = st.number_input('pH Level', min_value = 2.72, max_value = 3.9, value=3.2)
+feature10 = st.number_input('Sulphates Content', min_value = 0.6, max_value = 1.07, value=0.8)
+feature11 = st.number_input('Alcohol Content', min_value = 8.0, max_value = 14.9, value=10)
+
 colour = st.selectbox('Red or White:', ['Red','White'])
 
+# Calls the predict function from user input on button press
 if st.button('Predict Quality'):
-    quality = predict(feature1, feature2, feature3)
-    st.success(f'The predicted wine quality is {quality}')
+
+    # Collect user input
+    user_input = [feature1, feature2, feature3, feature4, feature5, feature6, feature7, feature8, feature9, feature10, feature11]
+    
+    quality = predict(user_input)
+    # quality = predict(feature1, feature2, feature3) # this will just be good/bad/declious?
+    st.success(f'The predicted wine quality is {quality}') # or we predict your wine to be [...]?
