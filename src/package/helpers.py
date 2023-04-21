7f08cef1f9ada5383a36cc6d5fc1886a454a6855
Ali T. Heidari <theidari.ali@gmail.com>
2023-04-20 23:41:44 -0400

    Update helpers.py
    cleaning helpers file

--- src/package/helpers.py
+++ src/package/helpers.py
@@ -3,40 +3,48 @@
 # --------------------------------------------------------------------------------------------------------------------------------------------
 
 # main dependencies and setup
+import os
 import numpy as np
 import pandas as pd
 from pandas_profiling import ProfileReport # data statistic profiling
 from pathlib import Path
+import datetime as datetime
+import statsmodels.api as sm
 
 # ml dependencies and setup
+from sklearn.pipeline import Pipeline # pipeline
 from sklearn.cluster import KMeans # KMeans
 from sklearn.decomposition import PCA # PCA
-from sklearn.preprocessing import StandardScaler # StandardScale to resize the distribution of values 
-from sklearn.metrics import silhouette_score # Silhouette method
-from sklearn.metrics import calinski_harabasz_score # Calinski Harabasz method
+from sklearn.preprocessing import StandardScaler, MinMaxScaler # StandardScale to resize the distribution of values 
+from sklearn.metrics import balanced_accuracy_score, confusion_matrix, classification_report, roc_curve, auc, r2_score, mean_squared_error, accuracy_score
+from sklearn import svm
+from sklearn.datasets import make_classification
+from sklearn.svm import SVC
+from sklearn.base import BaseEstimator, TransformerMixin # estimators and transformers 
 
-from sklearn.metrics import balanced_accuracy_score, confusion_matrix, classification_report
+from mord import LogisticAT
 
 # Import sampling 
-from sklearn.model_selection import train_test_split # Import the train_test_learn module
+from sklearn.model_selection import train_test_split, learning_curve # Import the train_test_learn module
 from imblearn.over_sampling import RandomOverSampler # Import the RandomOverSampler module form imbalanced-learn
 
 # Import models:
-from sklearn.linear_model import LogisticRegression # Logistic Regression
+from sklearn.linear_model import LogisticRegression, LinearRegression # Logistic Regression
 from sklearn.ensemble import RandomForestClassifier # RandomForest
 import xgboost as xgb # Extreme gradient boosting (XGBoost)
 import lightgbm as lgb # LightGBM
 
+import tensorflow as tf
+import keras_tuner as kt
 
-from sklearn.base import BaseEstimator, TransformerMixin # estimators and transformers 
-
-import statsmodels.api as sm
+# streamlit dependencies and setup
+import pickle
 
 # plotting dependencies and setup  
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import plotly.io as pio
-
+import matplotlib.pyplot as plt
 # package dependencies and setup
 from package.constants import * # constants
 
@@ -58,7 +66,7 @@ def data_splitter(df, test_size):
     train_df, test_df = train_test_split(df, test_size=test_size, random_state=42)
     train_df.reset_index(drop=True, inplace=True)
     test_df.reset_index(drop=True, inplace=True)
-    print(f"Number of Rows\ntrain: {train_df.shape[0]}\ntest: {test_df.shape[0]}")
+    print(f"Number of Rows in\ntrain: {train_df.shape[0]}\ntest: {test_df.shape[0]}")
     return train_df, test_df
 
 
@@ -75,20 +83,18 @@ def convert_and_profile(dfs):
     """
     for i, df in enumerate(dfs):
         # save train and test 
-        query_train = f"""train_{df}.to_csv('../data/splitdata/train_{df}.csv', index=False)"""
-        exec(query_train)
-        query_test = f"""test_{df}.to_csv('../data/splitdata/test_{df}.csv', index=False)"""
-        exec(query_test)
+        df_query= f"""{df}.to_csv('{DATA_URL}{df}.csv',index=False)"""
+        exec(df_query)
         # show report
-        profile_report = ProfileReport(locals()[f"train_{df}"], explorative=True, dark_mode=True)
-        profile_report_file_path = Path(f'../results/{df}_profile.html')
+        query_profile_report = f"""ProfileReport({df}, explorative=True, dark_mode=True)"""
+        profile_report = eval(query_profile_report)
+        profile_report_file_path = Path(f"{OUTPUT_URL}{df}_profile.html")
         try:
             profile_report_file_path.resolve(strict=True)
         except FileNotFoundError:
             profile_report.to_file(profile_report_file_path)
 
-            
-            
+                    
 def skewness_report(df):
     """
     Computes the skewness of each numerical column in a pandas DataFrame,
@@ -112,6 +118,7 @@ def skewness_report(df):
             skew_categories.append("High Skewness")
     result_df = pd.DataFrame({'c_name': df_features.columns, 'skewness': skew_values, 'category': skew_categories})
     return result_df
+
 
 # plotting function __________________________________________________________________________________________________________________________ 
 def violin_plot(df):
@@ -185,5 +192,9 @@ def violin_plot(df):
 
     return fig.show() # show the plot
 
-print(f"☑ helpers is imporetd")    
+
+# modeling functions _________________________________________________________________________________________________________________________
+
+
+print(f"☑ helpers have been imported")    
 # --------------------------------------------------------------------------------------------------------------------------------------------
